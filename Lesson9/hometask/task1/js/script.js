145d6cb5369c329753daa59c228712c2d5163315
neverm1ndz <roman.anhyshynets@gmail.com>
2023-03-16 13:32:56 +0200

    new

---
+++ Lesson9/hometask/task1/js/script.js
@@ -0,0 +1,157 @@
+// Знайти суми елементів у вказаній області
+
+function getRandomNum(minNum, maxNum) {
+   return minNum + Math.floor(Math.random() * (maxNum - minNum))
+}
+
+function generateArray(arrayLength, minNum, maxNum) {
+   let array = []
+   for (let i = 0; i < arrayLength; i++) {
+      const randomNum = getRandomNum(minNum, maxNum)
+      array.push(randomNum)
+   }
+   return array
+}
+
+function generateTable(tableLength, arrayLength, minNum, maxNum) {
+   let array = []
+   for (let i = 0; i < tableLength; i++) {
+      const randomColumn = generateArray(arrayLength, minNum, maxNum)
+      array.push(randomColumn)
+   }
+   return array
+}
+
+// 1
+
+// function getElementsSum_1(table, rows, columns) {
+//    const rowsHalf = Math.floor(rows / 2)
+//    const columnsHalf = Math.floor(columns / 2)
+//    let elementsSum = 0
+//    for (let i = 0; i < rowsHalf; i++) {
+//       for (let j = 0; j < columnsHalf; j++) {
+//          elementsSum += table[i][j]
+//       }
+//    }
+//    return elementsSum
+// }
+
+// 2
+
+// function getElementsSum_2(table, rows, columns) {
+//    const rowsHalf = Math.floor(rows / 2)
+//    const columnsHalf = Math.floor(columns / 2)
+//    let elementsSum = 0
+//    for (let i = 0; i < rowsHalf; i++) {
+//       for (let j = columnsHalf; j < columns; j++) {
+//          elementsSum += table[i][j]
+//       }
+//    }
+//    return elementsSum
+// }
+
+// 3
+
+// function getElementsSum_3(table, rows, columns) {
+//    const rowsHalf = Math.floor(rows / 2)
+//    const columnsHalf = Math.floor(columns / 2)
+//    let elementsSum = 0
+//    for (let i = rowsHalf; i < rows; i++) {
+//       for (let j = 0; j < columnsHalf; j++) {
+//          elementsSum += table[i][j]
+//       }
+//    }
+//    return elementsSum
+// }
+
+// 4
+
+// function getElementsSum_4(table, rows, columns) {
+//    const rowsHalf = Math.floor(rows / 2)
+//    const columnsHalf = Math.floor(columns / 2)
+//    let elementsSum = 0
+//    for (let i = rowsHalf; i < rows; i++) {
+//       for (let j = columnsHalf; j < columns; j++) {
+//          elementsSum += table[i][j]
+//       }
+//    }
+//    return elementsSum
+// }
+
+// 1,2,3,4
+
+function getElementsQarterAreaSum(table, startRows, startColumns) {
+   const endRows = startRows + table.length / 2
+   const endColumns = startColumns + table[0].length / 2
+   let elementsSum = 0
+   for (let i = startRows; i < endRows; i++) {
+      for (let j = startColumns; j < endColumns; j++) {
+         elementsSum += table[i][j]
+      }
+   }
+   return elementsSum
+}
+
+
+// 5 Суму парних рядків
+
+function getSumEvenRows(table) {
+   let evenRowsSum = 0
+   for (let i = 0; i < table.length; i += 2) {
+      for (let j = 0; j < table[i].length; j++)
+         evenRowsSum += table[i][j]
+   }
+   return evenRowsSum
+}
+
+// 6 Суму непарних стовпців
+
+function getSumOddColumns(table) {
+   let sumOddColumns = 0
+   for (let i = 0; i < table.length; i++) {
+      for (let j = 1; j < table[i].length; j += 2) {
+         sumOddColumns += table[i][j]
+      }
+   }
+   return sumOddColumns
+}
+
+// 7 У парних рядках – непарні стовпці, у непарних – парні.
+
+function getChessBoardElementsSum(table) {
+   let sum = 0
+   for (let i = 0; i < table.length; i++) {
+      if (i % 2 === 0)
+         for (let j = 1; j < table[i].length; j += 2) {
+            sum += table[i][j]
+         }
+      else
+         for (let j = 0; j < table[i].length; j += 2) {
+            sum += table[i][j]
+         }
+   }
+   return sum
+}
+
+const tableRows = parseInt(prompt('Введіть кількість рядків'))
+const tableColumns = parseInt(prompt('Введіть кількість стовбців'))
+const tableArray = generateTable(tableRows, tableColumns, 1, 10)
+const tableRowsHalf = Math.floor(tableRows / 2)
+const tableColumnsHalf = Math.floor(tableColumns / 2)
+
+const elementsSum_1 = getElementsQarterAreaSum(tableArray, 0, 0)
+const elementsSum_2 = getElementsQarterAreaSum(tableArray, 0, tableColumnsHalf)
+const elementsSum_3 = getElementsQarterAreaSum(tableArray, tableRowsHalf, 0)
+const elementsSum_4 = getElementsQarterAreaSum(tableArray, tableRowsHalf, tableColumnsHalf)
+const evenRowsSum = getSumEvenRows(tableArray)
+const oddColumnsSum = getSumOddColumns(tableArray)
+const chessBoardElemenstSum = getChessBoardElementsSum(tableArray)
+
+console.log(tableArray)
+console.log(`Сума в першій області = ${elementsSum_1}`)
+console.log(`Сума в другій області = ${elementsSum_2}`)
+console.log(`Сума в третій області = ${elementsSum_3}`)
+console.log(`Сума в третій області = ${elementsSum_4}`)
+console.log(`Сума елементів парних рядків = ${evenRowsSum}`)
+console.log(`Сума елементів непарних стовбців = ${oddColumnsSum}`)
+console.log(`Сума елементів у шахматному порядку = ${chessBoardElemenstSum}`)
---
+++ Lesson9/hometask/task3/js/script.js
@@ -0,0 +1,40 @@
+// Морський бій. Випадковим чином на двовимірному полі розміром 6*6 розташовується 5 кораблів. Користувач стріляє вказуючи координати. Гра продовжується поки не потоплено усі кораблі або у користувача не закінчаться снаряди.
+
+function getRandomNum(max) {
+   return Math.floor(Math.random() * max)
+}
+
+function createBattleField(shipsNum, fieldSize) {
+   let gameField = new Array(fieldSize).fill(0).map(() => new Array(fieldSize).fill(0))
+   for (let i = 0; i < shipsNum; i++) {
+      let row = getRandomNum(fieldSize)
+      let col = getRandomNum(fieldSize)
+      if (gameField[row][col] === 1) i--
+      else gameField[row][col] = 1
+   }
+   return gameField
+}
+
+function gameSeaBattle(battleField, shipsNum, shotsNum) {
+   let result
+   do {
+      alert(JSON.stringify(battleField))
+      let col = parseInt(prompt('Введіть номер стовбця (координати X від 1 до 6)'))
+      let row = parseInt(prompt('Введіть номер рядка (координати Y від 1 до 6)'))
+      shotsNum--
+      if (battleField[row - 1][col - 1] === 1) {
+         shipsNum--
+         alert(`Ви потопили корабель! Залишилось ${shipsNum} кораблів та ${shotsNum} снарядів`)
+      }
+      else alert(`Ви промахнулися! Залишилось ${shipsNum} кораблів та ${shotsNum} снарядів`)
+   } while (shipsNum > 0 && shotsNum > 0)
+   shipsNum > 0 ? result = 'Ви програли...' : result = 'Ви потопили всі кораблі!'
+   return result
+}
+
+const shipsNum = 5
+let shotsNum = parseInt(prompt('Введіть кількість снарядів'))
+let battleField = createBattleField(shipsNum, 6)
+console.log(battleField)
+
+alert(gameSeaBattle(battleField, shipsNum, shotsNum))
